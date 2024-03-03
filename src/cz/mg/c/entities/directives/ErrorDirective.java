2b6033fca786954a614f4d45bd72ac6a8fddd96d
Mg <gekoncze@seznam.cz>
2024-03-03 09:08:23 +0100

    added message field to error and warning directive

--- src/cz/mg/c/entities/directives/ErrorDirective.java
+++ src/cz/mg/c/entities/directives/ErrorDirective.java
@@ -1,15 +1,28 @@
 package cz.mg.c.entities.directives;
 
 import cz.mg.annotations.classes.Entity;
+import cz.mg.annotations.requirement.Optional;
+import cz.mg.annotations.storage.Value;
 import cz.mg.tokenizer.entities.tokens.WordToken;
 
 public @Entity class ErrorDirective extends Directive {
     public static final String KEYWORD = "error";
+
+    private String message;
 
     public ErrorDirective() {
     }
 
     public ErrorDirective(WordToken keyword) {
         super(keyword);
+    }
+
+    @Optional @Value
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
--- src/cz/mg/c/entities/directives/WarningDirective.java
+++ src/cz/mg/c/entities/directives/WarningDirective.java
@@ -1,15 +1,28 @@
 package cz.mg.c.entities.directives;
 
 import cz.mg.annotations.classes.Entity;
+import cz.mg.annotations.requirement.Optional;
+import cz.mg.annotations.storage.Value;
 import cz.mg.tokenizer.entities.tokens.WordToken;
 
 public @Entity class WarningDirective extends Directive {
     public static final String KEYWORD = "warning";
+
+    private String message;
 
     public WarningDirective() {
     }
 
     public WarningDirective(WordToken keyword) {
         super(keyword);
+    }
+
+    @Optional @Value
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
