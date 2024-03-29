0e2a1a3f7f8a7f8480cc686ce5210eca185da211
Mg <gekoncze@seznam.cz>
2024-03-29 10:41:01 +0100

    added bit field to variable

--- src/cz/mg/c/entities/CVariable.java
+++ src/cz/mg/c/entities/CVariable.java
@@ -11,12 +11,18 @@ import cz.mg.tokenizer.entities.Token;
 public @Entity class CVariable implements CMainEntity {
     private CType type;
     private String name;
+    private Integer bit;
     private List<Token> expression;
 
     public CVariable() {
     }
 
-    public CVariable(CType type, @Optional String name, @Optional List<Token> expression) {
+    public CVariable(
+        CType type,
+        String name,
+        Integer bit,
+        List<Token> expression
+    ) {
         this.type = type;
         this.name = name;
         this.expression = expression;
@@ -36,8 +42,17 @@ public @Entity class CVariable implements CMainEntity {
         return name;
     }
 
-    public void setName(@Optional String name) {
+    public void setName(String name) {
         this.name = name;
+    }
+
+    @Optional @Value
+    public Integer getBit() {
+        return bit;
+    }
+
+    public void setBit(Integer bit) {
+        this.bit = bit;
     }
 
     @Optional @Shared
