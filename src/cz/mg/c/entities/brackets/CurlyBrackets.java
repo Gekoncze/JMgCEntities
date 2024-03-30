f75c13ffbd1ca79064e20360f9b63ea033172e2b
Mg <gekoncze@seznam.cz>
2024-03-30 20:07:32 +0100

    moved classes to another project

--- src/cz/mg/c/entities/brackets/CurlyBrackets.java
+++
@@ -1,8 +0,0 @@
-package cz.mg.c.entities.brackets;
-
-import cz.mg.annotations.classes.Entity;
-
-public @Entity class CurlyBrackets extends Brackets {
-    public CurlyBrackets() {
-    }
-}
--- src/cz/mg/c/entities/brackets/RoundBrackets.java
+++
@@ -1,8 +0,0 @@
-package cz.mg.c.entities.brackets;
-
-import cz.mg.annotations.classes.Entity;
-
-public @Entity class RoundBrackets extends Brackets {
-    public RoundBrackets() {
-    }
-}
