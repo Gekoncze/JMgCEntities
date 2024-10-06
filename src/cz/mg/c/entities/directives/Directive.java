0e8bb29638aad41f10ea8dc16b292756ff153199
mg <gekoncze@seznam.cz>
2024-10-06 16:28:53 +0200

    directive is now c entity

--- src/cz/mg/c/entities/directives/Directive.java
+++ src/cz/mg/c/entities/directives/Directive.java
@@ -3,9 +3,10 @@ package cz.mg.c.entities.directives;
 import cz.mg.annotations.classes.Entity;
 import cz.mg.annotations.requirement.Required;
 import cz.mg.annotations.storage.Shared;
+import cz.mg.c.entities.CEntity;
 import cz.mg.token.tokens.WordToken;
 
-public abstract @Entity class Directive {
+public abstract @Entity class Directive implements CEntity {
     private WordToken keyword;
 
     public Directive() {
